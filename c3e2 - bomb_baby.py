285fd672b1ff8f1192de618181bc1c6ee14ca065
Ash McCallum <ash.mccallum@outlook.com>
2017-04-12 19:31:53 +0100

    Made easier when m/f = 1

--- c3e2 - bomb_baby.py
+++ c3e2 - bomb_baby.py
@@ -4,14 +4,23 @@ def answer(M, F):
 
     gen = 0
 
-    if (m != 1 or f != 1) and (m % f == 0) or (f % m == 0) or (m % 2 == 0 and f % 2 == 0) and (m < 1 and f < 1):
+    if ((m != 1 or f != 1) and (m % f == 0) or (f % m == 0) or (m % 2 == 0 and f % 2 == 0)) or (m < 1 or f < 1):
         return "impossible"
-    elif m > 10 ** 50 and f > 10 ** 50:
+    elif m > 10 ** 50 or f > 10 ** 50:
         return "impossible"
 
     while m != f:
-        if m < 1 and f < 1:
+        if m < 1 or f < 1:
             break
+        elif m == 1 or f == 1:
+            if m == 1:
+                gen += f - 1
+                f = 1
+                break
+            else:
+                gen += m - 1
+                m = 1
+                break
         elif m > f:
             m -= f
             gen += 1
@@ -19,14 +28,8 @@ def answer(M, F):
             f -= m
             gen += 1
 
-    if m == 1 or f == 1:
+    if m == 1 and f == 1:
         return gen
-    # elif m == 1:
-    #     gen += f - 1
-    #     return gen
-    # elif f == 1:
-    #     gen += m - 1
-    #     return gen
     else:
         return "impossible"
 
@@ -34,6 +37,6 @@ def answer(M, F):
 # print answer(2,1)
 # print answer(2,4)
 # print answer(4,7)
-# print answer(16,43)
+print answer(16,43)
 print answer(-3,1)
 print answer(10**10,47)
