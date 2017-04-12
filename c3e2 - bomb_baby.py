4de489229685cb3845aa6c62dc15efaafc58da8c
Ash McCallum <ash.mccallum@outlook.com>
2017-04-12 18:06:25 +0100

    Sorted incorrect operators

--- c3e2 - bomb_baby.py
+++ c3e2 - bomb_baby.py
@@ -6,7 +6,7 @@ def answer(M, F):
 
     if (m != 1 or f != 1) and (m % f == 0) or (f % m == 0) or (m % 2 == 0 and f % 2 == 0) and (m < 1 and f < 1):
         return "impossible"
-    elif m >= 10 ** 50 and f >= 10 * 50:
+    elif m > 10 ** 50 and f > 10 ** 50:
         return "impossible"
 
     while m != f:
