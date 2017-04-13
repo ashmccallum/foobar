9845fd407f6b71abd3c994214c9fc94c7eb0f4c6
Ash McCallum <ash.mccallum@outlook.com>
2017-04-13 10:55:07 +0100

    Replaced minus with divide for less loops
    SUCCESS!

--- c3e2 - bomb_baby.py
+++ c3e2 - bomb_baby.py
@@ -4,29 +4,25 @@ def answer(M, F):
 
     gen = 0
 
-    if ((m != 1 or f != 1) and (m % f == 0) or (f % m == 0) or (m % 2 == 0 and f % 2 == 0)) or (m < 1 or f < 1):
-        return "impossible"
-    elif m > 10 ** 50 or f > 10 ** 50:
-        return "impossible"
-
-    while m != f:
-        if m < 1 or f < 1:
+    while m != 1 or f != 1:
+        if m == f or m < 1 or f < 1:
             break
-        elif m == 1 or f == 1:
-            if m == 1:
-                gen += f - 1
-                f = 1
-                break
-            else:
-                gen += m - 1
-                m = 1
-                break
         elif m > f:
-            m -= f
-            gen += 1
+            multiplier = m // f
+            if f == 1:
+                gen += multiplier - 1
+                m -= (f * multiplier) - 1
+            else:
+                gen += multiplier
+                m -= f * multiplier
         else:
-            f -= m
-            gen += 1
+            multiplier = f // m
+            if m == 1:
+                gen += multiplier - 1
+                f -= (m * multiplier) - 1
+            else:
+                gen += multiplier
+                f -= m * multiplier
 
     if m == 1 and f == 1:
         return gen
@@ -34,9 +30,9 @@ def answer(M, F):
         return "impossible"
 
 
-# print answer(2,1)
-# print answer(2,4)
-# print answer(4,7)
-print answer(16,43)
-print answer(-3,1)
-print answer(10**10,47)
+print str(answer(2, 1)) + ", answer: 1"
+print str(answer(2, 4)) + ", answer: impossible"
+print str(answer(4, 7)) + ", answer: 4"
+print str(answer(16, 43)) + ", answer: 9"
+print str(answer(-3, 1)) + ", answer: impossible"
+print str(answer(10 ** 50, 47)) + ", answer: ???"
