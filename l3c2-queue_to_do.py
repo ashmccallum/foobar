be116bc80e1a8b1cd3d8d2151d3fbb22a33a34e0
ashmccallum <ashm@outlook.com>
2017-04-17 22:22:15 +0100

    completed queue to do

--- l3c2-queue_to_do.py
+++ l3c2-queue_to_do.py
@@ -10,21 +10,46 @@ def answer(start, length):
 
         # check starting number, even makes it easier
         if start % 2 == 0:
-            # if number of even-odd pairs is even, they cancel each other out, otherwise they return 1
-            if (i / 2) % 2 == 0:
-                workers.append(0)
+            if i % 2 == 0:
+                # if number of even-odd pairs is even, they cancel each other out, otherwise they return 1
+                if (i / 2) % 2 == 0:
+                    # even start, even number of items, even number of even-odd pairs
+                    workers.append(0)
+                else:
+                    # even start, even number of items, odd number of even-odd pairs
+                    workers.append(1)
             else:
-                workers.append(1)
-
-
-
+                if ((i - 1) / 2) % 2 == 0:
+                    if i == 1:
+                        workers.append(start)
+                    else:
+                        # even start, odd number of items, even number of even-odd pairs
+                        workers.extend([0, start + i])
+                else:
+                    # even start, odd number of items, odd number of even-odd pairs
+                    workers.extend([1, start + (i - 1)])
+        else:
+            if i % 2 == 0:
+                if (i / 2) % 2 == 0:
+                    # odd start, even number of items, even number of even-odd pairs
+                    workers.extend([start, 0, start + i])
+                else:
+                    if (i / 2) == 1:
+                        workers.extend([start, start + 1])
+                    else:
+                        # odd start, even number of items, odd number of even-odd pairs
+                        workers.extend([start, 1, start + i])
+            else:
+                if ((i - 1) / 2) % 2 == 0:
+                    # odd start, odd number of items, even number of even-odd pairs
+                    workers.extend([start, 0])
+                else:
+                    # odd start, odd number of items, odd number of even-odd pairs
+                    workers.extend([start, 1])
 
         start += length
 
-        # csArray.extend(row)
-    checksum = reduce(xor, workers)
-
-    return checksum
+    return reduce(xor, workers)
 
 
 print "output: " + str(answer(0,3)) + ", answer: 2"
